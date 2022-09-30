ec508c676861598229b56a640f666b620ba2ab67
Daniel Mermelstein <daniel.mermelstein@ibm.com>
2022-09-30 17:08:59 +0100

    elastic changes

--- elasticsearch/server.js
+++ elasticsearch/server.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 IBM Corp. All Rights Reserved.
+ * Copyright 2022 IBM Corp. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the “License”);
  * you may not use this file except in compliance with the License.
@@ -14,184 +14,94 @@
  * limitations under the License.
  */
 
-"use strict";
-/* jshint node:true */
-
 // Add the express web framework
 const express = require("express");
+const fs = require("fs");
 const app = express();
+const EC = require("./elasticclient.js")
+const INDEX = "words"
 
 // Use body-parser to handle the PUT data
 const bodyParser = require("body-parser");
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  bodyParser.json()
 );
 
-// Util is handy to have around, so thats why that's here.
-const util = require('util');
-// and so is assert
-const assert = require('assert');
-
-// Then we'll pull in the database client library
-let elasticsearch=require('@elastic/elasticsearch');
-
-// Now lets get cfenv and ask it to parse the environment variable
-let cfenv = require('cfenv');
-
-// load local VCAP configuration  and service credentials
-let vcapLocal;
-try {
-  vcapLocal = require('./vcap-local.json');
-  console.log("Loaded local VCAP");
-} catch (e) { 
-    // console.log(e)
-}
-
-const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {};
-const appEnv = cfenv.getAppEnv(appEnvOpts);
-
-// Within the application environment (appenv) there's a services object
-let services = appEnv.services;
-
-// The services object is a map named by service so we extract the one for Elasticsearch
-let esServices = services["databases-for-elasticsearch"];
-
-// This check ensures there is a services for Elasticsearch databases
-assert(!util.isUndefined(esServices), "Must be bound to databases-for-elasticsearch services");
-
-// We now take the first bound Elasticsearch service and extract it's credentials object
-let credentials = esServices[0].credentials.connection.https;
-
-// Within the credentials, an entry ca_certificate_base64 contains the SSL pinning key
-// We convert that from a string into a Buffer entry in an array which we use when
-// connecting.
-
-let client = new elasticsearch.Client( {
-  node: credentials.composed[0],
-  ssl: {
-      ca: Buffer.from(credentials.certificate.certificate_base64, 'base64')
-    }
-});
 
 // We want to extract the port to publish our app on
 let port = process.env.PORT || 8080;
 
-// We can now set up our web server. First up we set it to serve static pages
-app.use(express.static(__dirname + '/public'));
+// This is a global variable we'll use for handing the MongoDB client around
+let elasticdb;
 
-// Create the index if it doesn't already exist
-function checkIndices() {
-    client.indices
-      .exists({
-        index: "ibmclouddb"
-      })
-      .then(exists => {
-        if (exists === false) {
-          client.indices
-            .create({
-              index: "ibmclouddb",
-              body: {
-                mappings: {
-                  words: {
-                    properties: {
-                      word: { type: "text" },
-                      definition: { type: "text" },
-                      added: { type: "date" }
-                    }
-                  }
-                }
-              }
-            })
-            .catch(err => {
-              console.error(err);
-            });
-        }
-      })
-      .catch(err => {
-        console.error("Problem checking indices exist");
-    });
+// Add a word to the database
+async function addWord(word, definition) {
+  const body = {
+    word,
+    definition
+  }
+
+  const resp = await elasticdb.index({
+    index: INDEX,
+    body
+  })
+  //console.log(resp)
+  return {_id: resp.body._id, word, definition}
 }
 
-// Check for an existing index
-checkIndices();
+// Get words from the database
+async function getWords() {
 
-// Add a word to the index
-function addWord(word, definition) {
-    let now = new Date();
-    return client.index({
-      index: "ibmclouddb",
-      type: "words",
-      body: {
-        word: word,
-        definition: definition,
-        added: now
-      },
-      refresh: "wait_for"
-    });
-  }
+  const result = await elasticdb.search({
+    index: INDEX,
+    body: {
+      query: {
+        match_all: {}
+      }
+    }
+  })
 
-// Get words from the index
-function getWords() {
-    return client
-      .search({
-        index: "ibmclouddb",
-        type: "words",
-        _source: ["word", "definition"],
-        body: {
-          sort: {
-            added: {
-              order: "desc"
-            }
-          }
-        }
-      })
-      .then(results => {
-        return new Promise((resolve, reject) => {
-          let words = [];
-          results.body.hits.hits.forEach(function(hit) {
-            words.push({
-              word: hit._source.word,
-              definition: hit._source.definition
-            });
-          });
-          resolve(words);
-        });
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  }
+  //console.log(result.body.hits.hits)
+  return result.body.hits.hits.map(function (e) {
+    return {_id: e._id, word: e._source.word, definition: e._source.definition}
+  })
 
-// The user has clicked submit to add a word and definition to the index
+
+}
+
+// With the database going to be open as some point in the future, we can
+// now set up our web server. First up we set it to server static pages
+app.use(express.static(__dirname + "/public"));
+
+// The user has clicked submit to add a word and definition to the database
 // Send the data to the addWord function and send a response if successful
-app.put("/words", function(request, response) {
-    addWord(request.body.word, request.body.definition)
-        .then(function(resp) {
-            response.send(resp.body);
-        })
-        .catch(function(err) {
-            console.log(err);
-            response.status(500).send(err);
-        });
+app.put("/words", async function (request, response) {
+  try {
+    const resp = await addWord(request.body.word, request.body.definition)
+    response.send(resp);
+
+  } catch (err) {
+    console.log(err);
+    response.status(500).send(err);
+  }
+
 });
 
-
 // Read from the database when the page is loaded or after a word is successfully added
-// Use the getWords function to get a list of words and definitions from the index
-app.get("/words", function(request, response) {
-    getWords()
-        .then(function(words) {
-            response.send(words);
-        })
-        .catch(function(err) {
-            console.log(err);
-            response.status(500).send(err);
-        });
+// Use the getWords function to get a list of words and definitions from the database
+app.get("/words", async function (request, response) {
+  try {
+    const words = await getWords()
+    response.send(words);
+  } catch (err) {
+    console.log(err);
+    response.status(500).send(err);
+  }
 });
 
 // Listen for a connection.
-app.listen(port, function() {
-    console.log('Server is listening on port ' + port);
+app.listen(port, async function () {
+  //make the mongo connection
+  elasticdb = await EC()
+  console.log("Server is listening on port " + port);
 });
